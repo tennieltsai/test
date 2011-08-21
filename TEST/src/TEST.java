99a113b60cb814e24a0189c0fffb5b78cf6bd458
Tenniel Tsai <tenniel.tsai@gmail.com>
2011-08-21 16:38:48 +0800

    test

---
+++ TEST/src/TEST.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+
+public class TEST {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list = ["item"];
+        String item = list[0];
+        
+        for(String a :item)
+        {
+		System.out.print("test");
+        }
+	}
+
+}
